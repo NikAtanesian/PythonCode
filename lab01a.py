d2bfeae6380e4af21e8b08269f7059acdafc8c39
Nik Atanesian <atanesiannik@gmail.com>
2025-03-24 15:05:04 -0400

    My very first commit to GitHub

---
+++ lab01a.py
@@ -0,0 +1,11 @@
+"""
+Name: Nik Atanesian
+Date: 08/22/2023
+Program: Hello World Plus
+"""
+
+
+name = input("What is your name?")
+print(f"Hello {name}! I am delighted to meet you")
+myclass = "python"
+print(f"I hope you will learn to love {myclass} this semester")
